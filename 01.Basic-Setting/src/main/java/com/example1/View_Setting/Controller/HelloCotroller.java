567f242bd54a982f162fdffec00ef56c06d8142a
rkadl9999 <rkadl9999@naver.com>
2022-05-30 08:36:00 +0900

    기본 설정

---
+++ 01.Basic-Setting/src/main/java/com/example1/View_Setting/Controller/HelloCotroller.java
@@ -0,0 +1,15 @@
+package com.example1.View_Setting.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloCotroller {
+
+    @GetMapping("hello")
+    public String hello(Model model){
+        model.addAttribute("data","hello");
+        return "hello";
+    }
+}

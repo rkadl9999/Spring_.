0b8d5a37148db30945d6acb2ff6b42e45285da59
rkadl9999 <rkadl9999@naver.com>
2022-05-29 20:54:18 +0900

    01.view_setting

---
+++ View_Setting/src/main/java/com/example/view_setting/ViewSettingApplication.java
@@ -0,0 +1,14 @@
+package com.example.view_setting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViewSettingApplication {
+
+    public static void main(String[] args) {
+        System.out.print("View Setting Project");
+        SpringApplication.run(ViewSettingApplication.class, args);
+    }
+
+}
---
+++ View_Setting/src/test/java/com/example/view_setting/ViewSettingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.view_setting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViewSettingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

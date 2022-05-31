4d6518e3e6ee784ca0f339d3f447243303af0e60
rkadl9999 <rkadl9999@naver.com>
2022-05-31 23:36:56 +0900

    회원 관리 예제

--- Member_Controll_exam/src/test/java/com/example/Member_Controll_exam/repository/MemoryMemberRepositoryTest.java
+++ Member_Controll_exam/src/test/java/com/example/Member_Controll_exam/repository/MemoryMemberRepositoryTest.java
@@ -2,10 +2,18 @@ package com.example.Member_Controll_exam.repository;
 
 import com.example.Member_Controll_exam.domain.Member;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 class MemoryMemberRepositoryTest {
-    MemberRepository repository = new MemoryMemberRepository();
+    MemoryMemberRepository repository = new MemoryMemberRepository();
+
+    @AfterEach
+    public void afterEach(){
+        repository.clearStroe();
+    }
 
     @Test
     public void save(){
@@ -31,5 +39,20 @@ class MemoryMemberRepositoryTest {
         Member result = repository.findByName("Hyeonuk1").get();
 
         Assertions.assertThat(result).isEqualTo(member1);
+    }
+
+    @Test
+    public void findAll() {
+        Member member1 = new Member();
+        member1.setName("Hyeonuk1");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("Hyeonuk2");
+        repository.save(member2);
+
+        List<Member> result = repository.findAll();
+
+        Assertions.assertThat(result.size()).isEqualTo(2);
     }
 }

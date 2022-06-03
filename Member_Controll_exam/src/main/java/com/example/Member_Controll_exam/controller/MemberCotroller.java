659d6f0c2209f8116310da75b4b8df401277c599
rkadl9999 <rkadl9999@naver.com>
2022-06-04 00:00:58 +0900

    회원 관리 예제

--- Member_Controll_exam/src/main/java/com/example/Member_Controll_exam/controller/MemberCotroller.java
+++ Member_Controll_exam/src/main/java/com/example/Member_Controll_exam/controller/MemberCotroller.java
@@ -1,8 +1,14 @@
 package com.example.Member_Controll_exam.controller;
 
 import com.example.Member_Controll_exam.Service.MemberService;
+import com.example.Member_Controll_exam.domain.Member;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberCotroller {
@@ -11,5 +17,26 @@ public class MemberCotroller {
     @Autowired
     public MemberCotroller(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm(){
+        return "members/createMemberForm";
+    }
+    @PostMapping("/members/new")
+    public String create(MemberForm form){
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model){
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members",members);
+        return "members/memberList";
     }
 }

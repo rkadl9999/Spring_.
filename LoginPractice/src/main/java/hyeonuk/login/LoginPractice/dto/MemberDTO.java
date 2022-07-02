d748473e9ba607303f086a041720ab162896c124
rkadl9999 <rkadl9999@naver.com>
2022-07-03 00:44:54 +0900

    ✨ | MemberDTO 생성

---
+++ LoginPractice/src/main/java/hyeonuk/login/LoginPractice/dto/MemberDTO.java
@@ -0,0 +1,45 @@
+package hyeonuk.login.LoginPractice.dto;
+
+public class MemberDTO {
+    private String userid;
+    private String password;
+    private String name;
+    private String email;
+
+    public String getUserid() {
+        return userid;
+    }
+
+    public void setUserid(String userid) {
+        this.userid = userid;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString(){
+        return "MemberDTO [userid=" + userid + ", passwd=" + password + ", name=" + name + "]";
+    }
+}
